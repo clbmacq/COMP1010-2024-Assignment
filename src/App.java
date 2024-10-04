0aca279ef23da35855904a6419753911eada1ec4
MarkCMQ <mark.cai@students.mq.edu.au>
2024-10-04 15:52:29 +1000

    Updated Characters Stats
    Included and added new Players and introduced Players & Enemies

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        character Bob = new character("Bob", 10, 10, 10, 10);
-        Bob.getInfo();
+        character Bob = new character("Bob", 10, 10, "Shortsword", 10, 10, 10, "Enemy");
+        // Bob.getInfo();
+    
+        character caleb = new character("Caleb", 10, 10, "Sword", 10, 10, 10, "Player");
+        // caleb.getInfo();
+
+        character Jayden = new character("Jayden", 30, 30, "Machete",5, 7, 5, "Player");
+        // Jayden.getInfo();
+
+        character Mimo = new character("Mimo", 20, 20,"SwitchBlade", 5, 13, 15, "Enemy");
+        // Mimo.getInfo()
+
+        
+
+    
+
     }
 }
