d65173ebd7e9494786c3fab0f9a9b3c932884abc
clbmacq <161291370+clbmacq@users.noreply.github.com>
2024-10-25 15:05:01 +1100

    small change actionstest

--- src/main/ActionsTest.java
+++ src/main/ActionsTest.java
@@ -14,15 +14,21 @@ import org.junit.Test;
 
 
 public class ActionsTest {
+    public static void main(String[] args) {
+        // create objects
+        ArrayList<equipment> xx = new ArrayList<>();
+        character target = new character("healer" ,10, 10, 10, 10, "Enemy", xx);
+        // call healtest function
+        testHeal(target);
+    }
+
     @Test
     public void testAttack() {
       
     }
 
     @Test
-    public void testHeal() {
-        ArrayList<equipment> xx = new ArrayList<>();
-        character target = new character("healer" ,10, 10, 10, 10, "Enemy", xx);
+    public void testHeal(character target) {
         target.health = 5;
         int initialHealth = target.health;
         actions.heal(target);
